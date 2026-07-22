95bcc2abd36cb13b01278e45beadfb8a6afd7c2d
Amanyadav72 <amanyadavabhay@gmail.com>
2026-07-22 11:39:28 +0530

    Added login path in urls.py

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("product-form/", views.product_form, name="product_form"),
     path("product/<int:pk>/edit/", views.edit_product, name="edit_product"),
-    path("register/",views.register, name="register"  )
+    path("register/",views.register, name="register"  ),
+    path("login/", views.user_login, name="login")
 ]
