4dcd0e995988b44693b5058ef15899f7791da7ed
Amanyadav72 <amanyadavabhay@gmail.com>
2026-07-24 12:50:21 +0530

    Updated urls.py

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("register/",views.register, name="register"  ),
     path("login/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
+    path("change-password", views.chang_password, name="change_password")
 ]
