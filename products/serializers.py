ebb7a506260dfbc3ca31fa6916113ca88ff2e4f7
Amanyadav72 <amanyadavabhay@gmail.com>
2026-08-11 19:50:46 +0530

    updated serializers.py

--- products/serializers.py
+++ products/serializers.py
@@ -4,5 +4,5 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'image', 'status', 'categories', 'owner', 'created_at']
+        fields = ['id', 'name', 'description', 'price', 'image', 'status','is_available', 'categories', 'owner', 'created_at']
         read_only_fields = ['owner', 'created_at']
