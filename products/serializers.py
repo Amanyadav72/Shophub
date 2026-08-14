21e184c057ea5e4857a948acafdb9eaa41a0f08c
amanyadav72 <amanyadavabhay@gmail.com>
2026-08-14 11:54:48 +0530

    nested category serializer to show category

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name']
 
 class ProductSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True, read_only=True)
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'price', 'stock','image', 'status','is_available', 'categories', 'owner', 'created_at']
