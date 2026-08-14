1f57596f32757fcf38e016ac9ba7af262760d38e
amanyadav72 <amanyadavabhay@gmail.com>
2026-08-14 20:03:38 +0530

    pagination.py created

---
+++ products/pagination.py
@@ -0,0 +1,11 @@
+from rest_framework.pagination import PageNumberPagination
+
+class ShopHubPagination(PageNumberPagination):
+    # Default number of items per page if the frontend doesn't specify
+    page_size = 10 
+    
+    # Allows the React frontend to ask for a specific chunk size (e.g., ?page_size=20)
+    page_size_query_param = 'page_size' 
+    
+    # Hard limit to protect the database from memory crashes
+    max_page_size = 50
