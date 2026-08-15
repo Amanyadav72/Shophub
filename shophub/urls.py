7b47522c3d11c8622ad049d99434e44674d8e94c
amanyadav72 <amanyadavabhay@gmail.com>
2026-08-15 12:46:16 +0530

    updated shophub\urls.py to show api schema and docs

--- shophub/urls.py
+++ shophub/urls.py
@@ -18,9 +18,16 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # 1. This endpoint generates the raw JSON/YAML schema
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # 2. This endpoint provides the beautiful Swagger UI web page
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
     path("__debug__/", include("debug_toolbar.urls")),
     path('', include('products.urls')),
 ]
