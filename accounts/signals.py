b6c010fca98476d83f6aa8bc53ed83dfc185f874
amanyadav72 <amanyadavabhay@gmail.com>
2026-08-25 00:14:22 +0530

    Migration Complete, Problem faced during migration Unicode Truncation Windows Character Mapping fixed with Python built-in flag (-X utf8) and Signal Interference in accounts/signals.py fix added check for raw

--- accounts/signals.py
+++ accounts/signals.py
@@ -7,5 +7,9 @@ from .models import CustomerProfile
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_customer_profile(sender, instance, created, **kwargs):
+    # Ignore the signal if we are loading data from a fixture (loaddata)
+    if kwargs.get('raw', False):
+        return
+        
     if created:
         CustomerProfile.objects.create(user=instance)
