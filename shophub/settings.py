3a0ca8b7bc63401ee2bf559c4fe58dd890865a63
amanyadav72 <amanyadavabhay@gmail.com>
2026-09-04 00:56:26 +0530

    configured signals.py in app.py and products.apps.ProductsConfig in settings.py

--- shophub/settings.py
+++ shophub/settings.py
@@ -86,7 +86,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products',
+    'products.apps.ProductsConfig',
     'accounts.apps.AccountsConfig',
     'cart',
     'orders',
