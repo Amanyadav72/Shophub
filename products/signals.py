1e1eaa77102c4a584a288434f0118761d398b186
amanyadav72 <amanyadavabhay@gmail.com>
2026-09-04 00:51:45 +0530

    created signals.py in products for Cache Invalidation

---
+++ products/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import m2m_changed, post_delete, post_save
+from django.dispatch import receiver
+
+from .cache import invalidate_product_cache
+from .models import Product
+
+
+@receiver(post_save, sender=Product)
+def invalidate_product_cache_on_save(sender, instance, **kwargs):
+    invalidate_product_cache()
+
+
+@receiver(post_delete, sender=Product)
+def invalidate_product_cache_on_delete(sender, instance, **kwargs):
+    invalidate_product_cache()
+
+
+@receiver(
+    m2m_changed,
+    sender=Product.categories.through,
+)
+def invalidate_product_cache_on_category_change(sender, instance, action, **kwargs):
+    if action in {"post_add", "post_remove", "post_clear"}:
+        invalidate_product_cache()
